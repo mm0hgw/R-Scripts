a9f6c3004f74189c9fbc3e41da3c6fccf07e1f18
MavropaliasG <tefaa8@gmail.com>
2017-06-20 21:56:30 +0800

    changes

--- Scripts/Unemployment in the USA/Unemployment in the USA.R
+++ Scripts/Unemployment in the USA/Unemployment in the USA.R
@@ -1,3 +1,6 @@
+# This is code to produce a graph of the unemployment in the USA based on data from
+# the Federal Reserve Bank of St. Louis from 1967 to 2015
+
 # You will need packages ggplot2 and scales for this script to work
 library(ggplot2)
 library(scales)
@@ -5,7 +8,7 @@ library(scales)
 # we will use the dataset economics from the package ggplot2
 ch <- ggplot2::economics
 
-#we divide the unemployed with the total population to get the ratio as the y axis
+# we divide the unemployed with the total population to get the ratio as the y axis
 new.plot <- ggplot(ch, aes(x = ch$date, y = ch$unemploy/ch$pop))
 
 # We will produce the graph with x axis ticks every 2 years
@@ -15,14 +18,16 @@ new.plot + (scale_x_date(breaks = date_breaks("2 years"), name = "Year", labels 
   theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
 
   # now we format our y axis
-  scale_y_continuous(name = "Unemployed as % of US population", labels = percent, limits = c(0, 0.06), breaks = seq(0, 0.06, 0.01)) +
+  scale_y_continuous(name = "Unemployed as % of US population", labels = percent,
+                     limits = c(0, 0.06), breaks = seq(0, 0.06, 0.01)) +
 
  # here we add the title and subtitle to our graph
   ggtitle("Unemployment in the USA", subtitle = "Data from the Federal Reserve Bank of St. Louis from 1967 to 2015") +
   geom_point(alpha = 1)  +
 
   # here we draw the line of the graph
-  geom_smooth(se = F, color = "darkcyan", size = 1.2, method = 'loess') +
+  geom_smooth(se = F, color = "darkcyan",
+              size = 1.2, method = 'loess') +
 
   # lets change some visual aspects of the theme
   theme(panel.border = element_blank(),
