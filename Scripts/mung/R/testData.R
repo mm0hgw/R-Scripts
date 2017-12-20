6622113acb5ddc6724c429719138cc0b806bf37f
Dale <dale@piratepress.org>
2017-12-20 12:14:38 +0000

    mung package

---
+++ Scripts/mung/R/testData.R
@@ -0,0 +1,21 @@
+#' Test data for munging
+#'@doctype data
+#'@usage
+#'data(testData)
+#' @description
+#' Test data for your munging pleasure.
+#'@examples
+#'data(testData)
+#'mung(testData)
+"testData"
+
+#' Test data for munging
+#'@doctype data
+#'@usage
+#'data(testData)
+#' @description
+#' Test data for your munging pleasure.
+#'@examples
+#'data(testData)
+#'mung(testData2)
+"testData2"
