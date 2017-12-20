a7b0c6988c3a1bff9481e1ba4a43ca3bfb1a48a9
Dale <dale@piratepress.org>
2017-12-20 14:30:31 +0000

    fix grep call
    buildPackage mung 0.0.0.9001
    
    m

--- Scripts/mung/R/mung.R
+++ Scripts/mung/R/mung.R
@@ -1,17 +1,20 @@
 #'mung
 #'@description
 #' MUNG UNTIL NO GOOD
-#'@param x a data.frame, matrix or vector to mung.
+#'@param x a data.frame to mung.
 #'@param mungfuns a vector of function names to mung with.
 #'@param key a 'vector' mapping which columns mung together
 #'@return a 'matrix' containing the munged results.
 #'@export
 mung <- function(x, mungfuns = c("max", "min", "mean"), key = gsub("[\\.0-9]", "", 
     colnames(x))) {
+    if ("data.frame" != class(x)) 
+        x <- as.data.frame(x)
+    stopifnot('data.frame'==class(x))
     FUNS <- lapply(mungfuns, get)
     stopifnot(all("function" == sapply(FUNS, class)))
     out <- do.call(cbind, lapply(unique(key), function(y) {
-        key2 <- grep(y, key)
+        key2 <- grep(paste(sep = "", "^", y, "$"), key)
         out <- do.call(cbind, lapply(FUNS, function(MUNGFUN) {
             sapply(seq(nrow(x)), function(z) {
                 MUNGFUN(as.numeric(x[z, key2]))
@@ -26,3 +29,4 @@ mung <- function(x, mungfuns = c("max", "min", "mean"), key = gsub("[\\.0-9]", "
     }))
     as.data.frame(out)
 }
+
